09a2be6e4619ada4f15fe0efc1e8a34ed007d805
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-02-13 12:19:57 -0800

    Added oh-my-zsh install

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 ln -s "$PWD/zshrc" ~/.zshrc
 ln -s "$PWD/init.vim" ~/.config/nvim/init.vim
 ln -s "$PWD/avit-lambda.zsh-theme" ~/.oh-my-zsh/themes
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
