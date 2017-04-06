d5c3d147740bb510fb0b72748df1386fb4fdbc11
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-05 22:27:41 -0700

    Replace avit with agnoster

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+# This script assumes zsh, nvim, and git are already installed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs /
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.oh-my-zsh/themes/agnoster.zsh-theme --create-dirs \
+    https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme
 ln -s "$PWD/zshrc" ~/.zshrc
 ln -s "$PWD/init.vim" ~/.config/nvim/init.vim
-ln -s "$PWD/avit-lambda.zsh-theme" ~/.oh-my-zsh/themes
 ln -s "$PWD/gitconfig" ~/.gitconfig
 
