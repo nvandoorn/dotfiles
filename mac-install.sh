9f9e5950a43fa49a0734ede7d9b3017ed66d1051
Nick Van Doorn <nick@super.gd>
2020-01-19 10:20:59 -0800

    werk werk werk puttin in wurk OU

--- mac-install.sh
+++ mac-install.sh
@@ -31,6 +31,7 @@ ln -sfn $PWD/gitconfig $HOME/.gitconfig
 ln -sfn $PWD/gitignore_global $HOME/.gitignore_global
 ln -sfn $PWD/karabiner.json $HOME/.config/karabiner/karabiner.json
 ln -sfn $PWD/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
+ln -sfn $PWD/teamocil $HOME/.teamocil
 
 source $HOME/.zshrc
 nvm install --lts
