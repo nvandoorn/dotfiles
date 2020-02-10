f1f043e3f89b65c46a9568565f2885fd3be9f498
Nick Van Doorn <vandoorn.nick@gmail.com>
2020-02-09 17:51:20 -0800

    Organize my shit

--- personal.zsh
+++ personal.zsh
@@ -1,16 +1,6 @@
-alias cddotfiles='cd $HOME/Documents/dotfiles'
+alias cddotfiles='cd ~/personal/code/dotfiles'
 
-# backup helper function
-# TODO:
-# 1) Re-arrange this such that it can be run on a single folder
-#    (e.g make a folder in home that get's backed up and contains these)
-# 2) Make sure the permission mask is working properly. I recall having permission
-#    issues last time I restored from here
 function backup() {
-  rsync -rua --info=progress2 --exclude-from="$DOTFILES_PATH/rsync-ignore.txt" $HOME/Documents /Volumes/nick/backups
-  rsync -rua --info=progress2 --exclude-from="$DOTFILES_PATH/rsync-ignore.txt" $HOME/Wii /Volumes/nick/backups
-  rsync -rua --info=progress2 --exclude-from="$DOTFILES_PATH/rsync-ignore.txt" $HOME/Work /Volumes/nick/backups
-  rsync -rua --info=progress2 --exclude-from="$DOTFILES_PATH/rsync-ignore.txt" $HOME/Wallpapers /Volumes/nick/backups
-  rsync -rua --info=progress2 --exclude-from="$DOTFILES_PATH/rsync-ignore.txt" $HOME/Pictures /Volumes/nick/backups
+  rsync -rua --info=progress2 --exclude-from="$HOME/.dotfiles/rsync-ignore.txt" ~/personal /Volumes/nick/backups
 }
 
