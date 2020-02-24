0a1c3b6ea2deafb80634f2dc604e21279619110b
Nick Van Doorn <vandoorn.nick@gmail.com>
2020-02-24 11:30:31 -0800

    team bigger now

--- work.zsh
+++ work.zsh
@@ -1,4 +1,4 @@
 alias cdball='cd $HOME/work/smartballot'
 alias cdone='cd $HOME/work/one_feather'
-alias supergood='hub pull-request -r jarednorman -r alistairnorman'
+alias supergood='hub pull-request -r jarednorman -r alistairnorman -r adammathys -r omnistegan'
 
