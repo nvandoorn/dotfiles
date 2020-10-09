cc2048680f27a7707a03c89f928c7729947b9ca6
Nick Van Doorn <nick@super.gd>
2020-10-08 23:41:55 -0700

    q4

--- work.zsh
+++ work.zsh
@@ -1,3 +1,3 @@
 alias cdball='cd $HOME/work/smartballot'
 alias cdone='cd $HOME/work/one_feather'
-alias supergood='live -f && hub pull-request -r Noah-Silvera -r adammathys'
+alias supergood='live -f && hub pull-request -r Noah-Silvera -r omnistegan'
