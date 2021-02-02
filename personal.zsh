65afb662e6902c5f2a4637864622a406e43878e6
Nick Van Doorn <nick@super.gd>
2021-02-02 15:00:26 -0800

    Move keyboard source

--- personal.zsh
+++ personal.zsh
@@ -1,4 +1,5 @@
 alias cddotfiles='cd ~/.dotfiles'
+alias cdkey='cd ~/personal/code/qmk_firmware'
 
 function backup() {
   rsync -rua --info=progress2 --exclude-from="$HOME/.dotfiles/rsync-ignore.txt" ~/personal /Volumes/nick/backups
