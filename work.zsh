b06e08b06ce49429754112fd8c7eb2805e739828
Nick Van Doorn <nick@super.gd>
2021-02-02 15:00:34 -0800

    New review rotation!

--- work.zsh
+++ work.zsh
@@ -1,3 +1,3 @@
 alias cdball='cd $HOME/work/smartballot'
 alias cdone='cd $HOME/work/one_feather'
-alias supergood='live -f && hub pull-request -r Noah-Silvera -r omnistegan'
+alias supergood='live -f && hub pull-request -r "OneFeather/super-good-developers" -r Noah-Silvera'
