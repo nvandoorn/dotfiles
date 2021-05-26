d0b144a92cf015a51ca1d8896b23a781072173ec
Nick Van Doorn <nick@super.gd>
2021-05-26 01:05:38 -0700

    sry noah

--- work.zsh
+++ work.zsh
@@ -1,6 +1,6 @@
 alias cdball='cd $HOME/work/smartballot'
 alias cdone='cd $HOME/work/one_feather'
-alias one_pr='live && hub pull-request -r "OneFeather/super-good-developers" -r Noah-Silvera'
+alias one_pr='live && hub pull-request -r "OneFeather/super-good-developers"'
 alias rowan_pr='live && hub pull-request -r "watg/super-good-developers"'
 export DISABLE_HEADLESS_FEATURE_SPECS=true
 export CFLAGS="-Wno-error=implicit-function-declaration"
